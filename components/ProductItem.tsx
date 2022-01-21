e5228ee5e6f708985a2c949266f1bafe8fc01de1
guayana <isabelaguayanaz@gmail.com>
2022-01-21 17:24:28 -0300

    passando o ProductItem para memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,13 +1,17 @@
+import { memo } from 'react'
+
 interface ProductItem {
   id: number,
   price: number,
   title: string
 }
 
-export const ProductItem = ({id, price, title}: ProductItem) => {
+export const ProductItem = memo(({id, price, title}: ProductItem) => {
   return(
     <div>
       <span>{id}</span> - <strong>{title}</strong> - <span>{price}</span>
     </div>
   )
-}
+}, (prevProps, nextProps) => {
+  return Object.is(prevProps, nextProps)
+})
